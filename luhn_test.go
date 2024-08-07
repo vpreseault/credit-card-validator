d2a80ceba6750b2e1006ead7f82e810877416b91
vpreseault <vincent.preseault@gmail.com>
2024-08-07 08:56:58 -0400

    add tests to stringToIntSlice

--- luhn_test.go
+++ luhn_test.go
@@ -23,3 +23,32 @@ func TestIsValidLuhn(t *testing.T) {
         }
     }
 }
+
+func TestStringToIntSlice(t *testing.T) {
+	testCases := []struct {
+        s string;
+		expected []int;
+    }{
+    	{"5555555555554444", []int{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4}},
+		{"4012888888881881", []int{4, 0, 1, 2, 8, 8, 8, 8, 8, 8, 8, 8, 1, 8, 8, 1}},
+		{"4111111111111111", []int{4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}},
+		{"6011000990139424", []int{6, 0, 1, 1, 0, 0, 0, 9, 9, 0, 1, 3, 9, 4, 2, 4}},
+    	{"17893729974", []int{1, 7, 8, 9, 3, 7, 2, 9, 9, 7, 4}},
+    	{"17893729977", []int{1, 7, 8, 9, 3, 7, 2, 9, 9, 7, 7}},
+		{"4012888888888888", []int{4, 0, 1, 2, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8}},
+    }
+
+    for _, tc := range testCases {
+        result, _ := stringToIntSlice(tc.s)
+
+		if len(result) != len(tc.expected) {
+			t.Errorf("stringToIntSlice(%v) = %v; want %v", tc.s, result, tc.expected)
+		}
+		
+		for i := 0; i < len(result); i++ {
+			if result[i] != tc.expected[i] {
+				t.Errorf("stringToIntSlice(%v) = %v; want %v", tc.s, result, tc.expected)
+			}
+		}
+    }
+}
